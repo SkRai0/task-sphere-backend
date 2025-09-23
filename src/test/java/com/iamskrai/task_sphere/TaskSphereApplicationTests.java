1d8eec44b26485711895263f57c5c507081cb9a8
SkRai <sudhanshurai025@gmail.com>
2025-09-23 14:53:19 +0530

    Initial Skeleton Setup

---
+++ src/test/java/com/iamskrai/task_sphere/TaskSphereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iamskrai.task_sphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskSphereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
